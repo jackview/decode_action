6129fed4faacec18c508fd3afe9e15c7083a9545
action <action@github.com>
2024-08-30 10:00:31 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Aug 30 2024 09:50:12 GMT+0000 (Coordinated Universal Time)
+//Fri Aug 30 2024 10:00:31 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 NAME = "星芽免费短剧";
